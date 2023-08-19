dc204b38549a3b00470bb61ba2031c8c192f7a7b
Aman Raj <amanraj749@gmail.com>
2023-08-19 16:47:56 +0530

    portfolio

---
+++ src/data/data.js
@@ -0,0 +1,28 @@
+
+
+export const data = [
+  // {
+  //   id: 1,
+  //   img: 'https://i.postimg.cc/VkRWdHWx/57214s81.jpg',
+  //   title: "Multimedia Web",
+  //   desc: "",
+  //   link: "",
+  //   git: "",
+  // },
+  {
+    id: 2,
+    img: 'https://i.postimg.cc/XYLzqXD3/Screenshot-2023-08-19-163640.png',
+    title: "Flight Booking",
+    link: "",
+    git: "https://github.com/aRaj446/online-flight-booking",
+  },
+  {
+    id: 3,
+    img: 'https://i.postimg.cc/vBvwJ4sF/Screenshot-2023-08-18-235033.png',
+    title: "Puppy Match",
+    desc: "",
+    link: "",
+    git: "https://github.com/aRaj446/puppy_match",
+  }
+  
+];
